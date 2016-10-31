69ea6f2a601c1ec6e4ef0ad25d0300397e4be20b
Erik Marstaeller <erik.marstaeller@denkwerk.com>
2016-10-31 17:22:49 +0100

    included cli input to configure paths

--- index.js
+++ index.js
@@ -1,116 +1,132 @@
-var glob = require('glob')
+var glob = require('glob');
 var fs = require('fs');
 var nunjucks = require('nunjucks');
+var prompt = require('prompt');
+
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'));
 NODE_DEBUG = fs, nunjucks, glob;
 var navi = '';
 var inputPath = '../../htdocs/frontend/dist/npm-dw-styleguide/markup/';
-var inputPath = 'markup-full/';
 if (process.env.npm_config_input) {
     inputPath = process.env.npm_config_input;
 }
 var docPath = '../../htdocs/frontend/dist/npm-dw-styleguide/doc/'
-var docPath = 'doc/'
 if (process.env.npm_config_doc) {
     docPath = process.env.npm_config_doc;
 }
 var outputPath = '../../htdocs/frontend/dist/npm-dw-styleguide/reduced/';
-var outputPath = 'reduced/';
 if (process.env.npm_config_output) {
     outputPath = process.env.npm_config_output;
 }
 
-//read html files
-glob('**/*.html', {
-    cwd: inputPath
-}, function(err, files) {
+prompt.start();
+
+prompt.get(['inputPath', 'docPath', 'outputPath'], function(err, result) {
     if (err) {
-        return log(er);
+        return onErr(err);
     }
-    var allTypes = files.map(function(file) {
-        return file.match(/^([\w\d-_]+)\//)[1];
-    }).reduce(function(pre, cur) {
-        if (!~pre.indexOf(cur)) {
-            pre.push(cur);
+    console.log('Command-line input received:');
+    console.log('  inputPath: ' + result.inputPath);
+    console.log('  docPath: ' + result.docPath);
+    console.log('  Email: ' + result.outputPath);
+    (!result.inputPath) ? null: inputPath = result.inputPath;
+    (!result.docPath) ? null: docPath = result.docPath;
+    (!result.outputPath) ? null: outputPath = result.outputPath;
+    style(inputPath, docPath, outputPath);
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
+
+//read html files
+function style(inputPath, docPath, outputPath) {
+    glob('**/*.html', {
+        cwd: inputPath
+    }, function(err, files) {
+        if (err) {
+            return log(er);
         }
-        return pre;
-    }, []);
-    //build structure for navigation
-    var navTree = [];
+        console.log(inputPath);
 
-    var buildNav = function(splitPath, children) {
-        var rawItem = splitPath.shift();
-        if (splitPath.length === 0) {
-            children.push({
-                name: rawItem,
-                link: rawItem.split('.')[0]
-            });
-        } else {
-            var item = children.filter(function(item) {
-                return item.name === rawItem;
-            })[0];
+        //build structure for navigation
+        var navTree = [];
 
-            if (!item) {
-                item = {
+        var buildNav = function(splitPath, children) {
+            var rawItem = splitPath.shift();
+            if (splitPath.length === 0) {
+                children.push({
                     name: rawItem,
-                    children: []
-                };
+                    link: rawItem.split('.')[0]
+                });
+            } else {
+                var item = children.filter(function(item) {
+                    return item.name === rawItem;
+                })[0];
 
-                children.push(item);
+                if (!item) {
+                    item = {
+                        name: rawItem,
+                        children: []
+                    };
+
+                    children.push(item);
+                }
+
+                buildNav(splitPath, item.children);
             }
-
-            buildNav(splitPath, item.children);
         }
-    }
-    files.forEach(function(file) {
-        var element = file.split('/');
-        var tree = buildNav(element, navTree);
-    })
-
-    var fileContents = [];
-
-    files.forEach(function(file, i, ar) {
-        var split = file.split("/");
-
-        var contents = fs.existsSync(inputPath + file) ?
-            fs.readFileSync(inputPath + file) : '';
-        var doc = (fs.existsSync(docPath + file.split('.')[0] + '.md')) ?
-            fs.readFileSync(docPath + file.split('.')[0] + '.md') :
-            'Bitte Dokumentation für ' + file + ' anlegen';
-        fileContents.push({
-            name: split[split.length - 1]
-                .split(".")[file.split(".").length - 2],
-            content: contents.toString(),
-            doc: doc,
-            path: file
+        files.forEach(function(file) {
+            var element = file.split('/');
+            var tree = buildNav(element, navTree);
         })
-    })
-    env.addGlobal('renderSingle', function(inputPath) {});
 
-    var full = env.render('full.njk', {
-        title: 'Full styleguide',
-        elements: fileContents,
-        navigation: navTree
-    });
-    fs.writeFile('index.html', full);
+        var fileContents = [];
 
-    fs.existsSync(outputPath) ? null : fs.mkdirSync(outputPath);
+        files.forEach(function(file, i, ar) {
+            var split = file.split("/");
 
-    fileContents.forEach(function(file, i) {
-        var reduced = env.render('reduced.njk', {
-            title: 'Reduced Module ' + file.name,
-            element: file
+            var contents = fs.existsSync(inputPath + file) ?
+                fs.readFileSync(inputPath + file) : '';
+            var doc = (fs.existsSync(docPath + file.split('.')[0] + '.md')) ?
+                fs.readFileSync(docPath + file.split('.')[0] + '.md') :
+                'Bitte Dokumentation für ' + file + ' anlegen';
+            fileContents.push({
+                name: split[split.length - 1]
+                    .split(".")[file.split(".").length - 2],
+                content: contents.toString(),
+                doc: doc,
+                path: file
+            })
+        })
+        env.addGlobal('renderSingle', function(inputPath) {});
+
+        var full = env.render('full.njk', {
+            title: 'Full styleguide',
+            elements: fileContents,
+            navigation: navTree
         });
-        paths = file.path.split('/');
-        var p = '';
+        fs.writeFile('index.html', full);
 
-        paths.forEach(function(path, i, paths) {
-            if (i + 1 < paths.length) {
-                p += path + '/';
-                fs.existsSync(outputPath + p) ? null : fs.mkdirSync(outputPath + p);
-            }
+        fs.existsSync(outputPath) ? null : fs.mkdirSync(outputPath);
+
+        fileContents.forEach(function(file, i) {
+            var reduced = env.render('reduced.njk', {
+                title: 'Reduced Module ' + file.name,
+                element: file
+            });
+            paths = file.path.split('/');
+            var p = '';
+
+            paths.forEach(function(path, i, paths) {
+                if (i + 1 < paths.length) {
+                    p += path + '/';
+                    fs.existsSync(outputPath + p) ? null : fs.mkdirSync(outputPath + p);
+                }
+            })
+            fs.writeFile(outputPath + file.path, reduced);
         })
-        fs.writeFile(outputPath + file.path, reduced);
     })
-})
+}
 module.exports = 'npm-dw-styleguide';
